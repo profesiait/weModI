dbe4d927e286e01e92ed9803c15812382a60b0e8
Gabriele Gianoglio <gabriele.gianoglio@profesia.it>
2024-04-16 17:36:55 +0200

    Dependencies

--- PDNDProviderJWT/src/main/java/it/profesia/wemodi/identity/oauth2/token/IdAuditRest01GrantValidator.java
+++
@@ -1,8 +0,0 @@
-package it.profesia.wemodi.identity.oauth2.token;
-
-import javax.servlet.http.HttpServletRequest;
-import org.apache.oltu.oauth2.common.validators.AbstractValidator;
-
-public class IdAuditRest01GrantValidator extends AbstractValidator<HttpServletRequest> {
-
-}
